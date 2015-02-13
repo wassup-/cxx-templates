0fb9aeda630573504a18b79caec101722eee95d5
Tom Knapen <tom_knapen@student.uhasselt.be>
2015-02-13 16:01:14 +0100

    modified formatting

--- sfinae.hpp
+++ sfinae.hpp
@@ -1,9 +1,17 @@
 #ifndef SFINAE_INCLUDED_
 #define SFINAE_INCLUDED_
 
-namespace sfinae {
-	typedef struct { char _; } yes;
-	typedef struct { yes _[2]; } no;
-}
+namespace tmpl
+{
+
+namespace sfinae
+{
+
+typedef struct { char _; } yes;
+typedef struct { yes _[2]; } no;
+
+} // namespace sfinae
+
+} // namespace tmpl
 
 #endif
